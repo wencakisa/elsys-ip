f4dbf9e78eceb7b45f7cd43c5a28c8ec678bcbe3
wencakisa <wencakisa@gmail.com>
2018-12-09 21:40:09 +0200

    Added comments explaining usage of metacharacters where needed

--- 04-regex/input.js
+++ 04-regex/input.js
@@ -2,9 +2,21 @@ regExps = {
   'exercise_1': /[A-Z][a-z]+/,
   'exercise_2': /088[1-9]\d{6}/,
   'exercise_3': /[^01]+/,
-  'exercise_4': /^\w[\w\d._]{2,13}$/,
+  // Using \w regex metacharacter
+  // which represents the following symbols at once:
+  // [a-zA-Z_0-9]
+  // and it fits perfect for the use case.
+  'exercise_4': /^[a-zA-Z][\w.]{2,13}$/,
   'exercise_5': /^(\d{1,3}|1[0-4]\d{2}|1500)$/,
-  'exercise_6': /class=['"](.*?)['"]/
+  // According to HTML class name specification:
+  // Naming rules:
+  // -> Must begin with a letter A-Z or a-z
+  // -> Can be followed by:
+  //    -> letters (A-Za-z),
+  //    -> digits (0-9)
+  //    -> hyphens ("-")
+  //    -> and underscores ("_")
+  'exercise_6': /class=['"][a-zA-Z][\w- ]+['"]/
 };
 
 cssSelectors = {
